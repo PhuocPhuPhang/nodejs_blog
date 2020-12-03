0233a38cc427947755aa09789f08abae1edde087
Phuoc Nguyen <nguyenhuuphuocbt98@gmail.com>
2020-12-03 22:34:33 +0700

    install Nodemon, Morgan

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.use(morgan('combined')) // Sử dụng Morgan de check HTTP request
+
+app.get('/home', (req, res) => {
   res.send('Hello World!')
 })
 
