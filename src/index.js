c655bde8108252ca86018215b15d46182ba40fbf
Phuoc Nguyen <nguyenhuuphuocbt98@gmail.com>
2020-12-07 22:30:50 +0700

    Form Data with method Post

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const app = express();
 const port = 3000;
 
 // Use static files
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(morgan("combined")); // HTTP Logger
+// app.use(morgan("combined")); // HTTP Logger
 
 // Template Engine
 app.engine(
@@ -23,8 +23,25 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 // console.log('Path: ', path.join(__dirname, 'resources/views'));
 
+// Parse Form Data post
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json()); // Áp dụng cho các dạng Submit Form = ajax, XMLHttpRequest, axios,...
+
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.post("/search", (req, res) => {
+  console.log(req.body); // body là dữ liệu của Form Data
+  res.send("");
 });
 
 app.listen(port, () => {
